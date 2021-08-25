75e690a94bfab465c6d2f61b63f644b034421e6f
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2021-08-25 13:22:07 +0530

    channel -> model, controller and route implimented

--- Controllers/channel.controller.js
+++ Controllers/channel.controller.js
@@ -1,1 +1,62 @@
-console.log("channel controller");
+const Cslot = require('../Models/channel.model')
+
+//Create Slot
+const createSlot = async (req, res) => {
+  if (req.body) {
+    const cslot = new Cslot(req.body)
+    await cslot
+      .save()
+      .then(data => {
+        res.status(200).send({ data: data })
+      })
+      .catch(error => {
+        res.status(500).send({ error: error.message })
+      })
+  }
+}
+
+//Get All Slot
+const getAllSlots = async (req, res) => {
+  await Cslot.find({})
+    .then(data => {
+      res.status(200).send({ data: data })
+    })
+    .catch(error => {
+      res.status(500).send({ error: error.message })
+    })
+}
+
+//Get Specific Slot
+const specificSlot = async (req, res) => {
+  let cslotid = req.params.id
+  await Cslot.findById(cslotid, (error, cslot) => {
+    if (error) {
+      return res.status(500).json({ success: false, error })
+    }
+    return res.status(200).json({
+      success: true,
+      cslot
+    })
+  })
+}
+
+//delete Specific Slot
+const deleteSpecificSlot = async (req, res) => {
+  let cslotid = req.params.id
+  await Cslot.findByIdAndRemove(cslotid).exec((error, deleteSlot) => {
+    if (error) {
+      return res.status(500).json({ success: false, error })
+    }
+    return res.status(200).json({
+      success: true,
+      deleteSlot
+    })
+  })
+}
+
+module.exports = {
+  createSlot,
+  getAllSlots,
+  specificSlot,
+  deleteSpecificSlot
+}
--- Routes/channel.route.js
+++ Routes/channel.route.js
@@ -1,1 +1,11 @@
-console.log("channel route");
+const express = require('express')
+const router = express.Router()
+const controller = require('../Controllers/channel.controller')
+
+module.exports = function () {
+  router.post('/create', controller.createSlot)
+  router.get('/', controller.getAllSlots)
+  router.get('/:id', controller.specificSlot)
+  router.delete('/delete/:id', controller.deleteSpecificSlot)
+  return router
+}
