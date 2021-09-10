635653a62c46726c021de96257b0020d81797bfd
Maduka Nuwantha <mbnuwantha90@gmail.com>
2021-09-11 04:13:29 +0530

    Server.js edited in dog and cat routes

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const config = require("config");
  * Route Imports
  *****************************************************************************************************************************/
 const daycareRoute = require("./Routes/daycare.route");
+const dogRoute = require("./Routes/dog.route");
+const catRoute = require("./Routes/cat.route");
 const userRoute = require("./Routes/user.route");
 const cslot = require("./Routes/channel.route");
 const capp = require('./Routes/appointment.route');
@@ -34,9 +36,10 @@ connection.once("open", () => {
  * Routes
  *****************************************************************************************************************************/
 router.use("/daycare", daycareRoute);
+router.use("/dog", dogRoute);
+router.use("/cat", catRoute);
 router.use("/user", userRoute);
-router.use("/public/profile_pictures",express.static('./public/profile-pictures'));
-
+router.use("/public/profile_pictures", express.static('./public/profile-pictures'));
 app.use('/cslot', cslot());
 app.use('/capp', capp());
 
