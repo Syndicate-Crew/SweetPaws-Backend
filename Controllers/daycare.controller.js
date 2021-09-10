5d9cc5bbb6266ff752ea9e25197258991e27d1f6
DilushaC <dilushafernando97@gmail.com>
2021-09-10 19:02:46 +0530

    Daycare update function implement

--- Controllers/daycare.controller.js
+++ Controllers/daycare.controller.js
@@ -47,8 +47,34 @@ const getDaycareById = async (req, res) => {
         });
 };
 
+const updateDaycare = async (req, res) => {
+    await Daycare.findOneAndUpdate({ _id: req.params.id }, req.body).
+        then(result => {
+            if (result == null) {
+                res.json({
+                    status: "Unsuccessful!",
+                    description: "No Daycare Found!"
+                });
+            } else {
+                Daycare.findOne({ _id: req.params.id }).then(result => {
+                    res.status(200).json({
+                        status: "Successful!",
+                        results: result
+                    })
+                })
+            }
+        })
+        .catch(err => {
+            res.json({
+                status: "Error!",
+                description: err
+            });
+        });
+}
+
 module.exports = {
     createDaycare,
     getAllDaycares,
-    getDaycareById
+    getDaycareById,
+    updateDaycare
 };
