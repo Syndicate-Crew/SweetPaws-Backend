c90342471f2f7e5d37748ff267a1eb5b65aef467
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2021-09-17 18:23:32 +0530

    channelling -> Get Appointment Actions

--- Controllers/appointment.controller.js
+++ Controllers/appointment.controller.js
@@ -23,9 +23,42 @@ const getAllApps = async (req,res) => {
     });
 }
 
+//get only pending appointment
 
+const getPendingApps = async (req,res) => {
+    await Capp.find({action:"Pending"})  
+    .then(data =>{
+        res.status(200).send({ data: data });
+    })
+    .catch(error => {
+        res.status(500).send({ error: error.message });
+    });
+}
+
+//Update Specific Appointment
+
+const updateApp = async (req,res) => {
+    await Capp.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (error)=>{
+            if(error){
+                return res.status(500).json({
+                    error:error
+                });
+            }
+            return res.status(200).json({
+                success:"Update Successfully"
+            })
+        }
+    );
+}
 
 module.exports = {
     createApp,
-    getAllApps
+    getAllApps,
+    getPendingApps,
+    updateApp,
 };
--- Routes/appointment.route.js
+++ Routes/appointment.route.js
@@ -5,5 +5,7 @@ const controller = require('../Controllers/appointment.controller')
 module.exports = function () {
   router.post('/create', controller.createApp)
   router.get('/', controller.getAllApps)
+  router.get('/pending', controller.getPendingApps)
+  router.put('/update/:id', controller.updateApp)
   return router
 }
