42d86da01efcd108859fc3fe21bf58a5a45942ed
MalinduPanchala <malindupanchala@gmail.com>
2021-10-01 18:21:02 +0530

    user.routes updated
    Routes for update functions implemented

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -7,6 +7,8 @@ router.route("/").get(controller.get);
 
 router.route("/checkUser").get(controller.validate);
 
+router.route("/getByName/:name").get(controller.getByName);
+
 router.route("/:id").get(controller.getById);
 
 router.route("/SignIn").post(controller.signIn);
@@ -15,7 +17,11 @@ router.route("/SignUp").post(upload.single("image"),controller.create);
 
 router.route("/auth").post(auth, controller.auth)
 
-router.route("/").put([auth, upload.single("image")], controller.update)
+router.route("/imageUpdate").put([auth, upload.single("image")], controller.updateImage);
+
+router.route("/infoUpdate").put(auth, controller.updateInfo);
+
+router.route("/passwordUpdate").put(auth, controller.changePassword);
 
 module.exports = router;
 
